af4e3f5a6b985be4e7dcacb08432d3166eab82c6
EONMeds Deploy <deploy@eonmeds.com>
2026-01-05 20:45:58 -0500

    Remove Spanish language toggle - English only

--- src/components/ClientProviders.tsx
+++ src/components/ClientProviders.tsx
@@ -2,12 +2,10 @@
 
 import { ReactNode } from 'react';
 import { LanguageProvider } from '@/contexts/LanguageContext';
-import LanguageToggle from '@/components/LanguageToggle';
 
 export default function ClientProviders({ children }: { children: ReactNode }) {
   return (
     <LanguageProvider>
-      <LanguageToggle />
       {children}
     </LanguageProvider>
   );
